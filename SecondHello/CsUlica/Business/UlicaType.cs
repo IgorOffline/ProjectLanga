c5a3272ad567974e75345148368db10e9bc1dff6
Igor Durbek <igor@igordurbek.com>
2024-09-25 13:23:06 +0200

    Ulica print programId INIT

---
+++ SecondHello/CsUlica/Business/UlicaType.cs
@@ -0,0 +1,8 @@
+namespace CsUlica.Business;
+
+public enum UlicaType : ushort
+{
+    Object = 0,
+    Bigint = 1,
+    String = 2
+}
---
+++ langh/src/test/java/practice/langh/LanghApplicationTests.java
@@ -0,0 +1,13 @@
+package practice.langh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanghApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
